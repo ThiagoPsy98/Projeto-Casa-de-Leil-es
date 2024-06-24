49bd9c8f481223e55b25e1cb0de5c02531447119
Thiago Santos <editorthiago74@gmail.com>
2024-06-24 12:45:51 -0700

    Conectando banco de dados

--- src/conectaDAO.java
+++ src/conectaDAO.java
@@ -6,23 +6,16 @@ import javax.swing.JOptionPane;
 
 
 
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-
-/**
- *
- * @author Adm
- */
 public class conectaDAO {
-    
+    private static String url = "jdbc:mysql://localhost:3306/uc11?useSSL=false";
+    private static String user = "root";       
+    private static String password = "Mineclash2395";     
     public Connection connectDB(){
         Connection conn = null;
         
         try {
         
-            conn = DriverManager.getConnection("jdbc:mysql://localhost/uc11?user=root&password=");
+            conn = DriverManager.getConnection(url, user, password);
             
         } catch (SQLException erro){
             JOptionPane.showMessageDialog(null, "Erro ConectaDAO" + erro.getMessage());
